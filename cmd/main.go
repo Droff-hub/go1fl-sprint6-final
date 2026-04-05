1161b1126632e4ef29369462430305fe31ea366d
Дорофеев Дмитрий <dimadrofik@yandex.com>
2026-04-05 21:20:02 +0400

    feat: implement morse converter with HTTP server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/golf1-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("Запуск сервера на http://localhost:8080")
+
+	err := srv.Server.ListenAndServe()
+	if err != nil {
+		logger.Fatalf("Ошибка при запуске сервера: %v", err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,45 @@
 package service
+
+import (
+	"strings"
+	"unicode"
+
+	"github.com/Yandex-Practicum/golf1-sprint6-final/pkg/morse"
+)
+
+func Convert(input string) (string, error) {
+	input = strings.TrimSpace(input)
+
+	if input == "" {
+		return "", nil
+	}
+
+	isMorse := true
+	for _, ch := range input {
+		if ch != '.' && ch != '-' && ch != ' ' && ch != '\n' && ch != '\r' {
+			isMorse = false
+			break
+		}
+	}
+
+	hasLetter := false
+	for _, ch := range input {
+		if unicode.IsLetter(ch) {
+			hasLetter = true
+			break
+		}
+	}
+
+	if hasLetter {
+		isMorse = false
+	}
+
+	var result string
+	if isMorse {
+		result = morse.ToText(input)
+	} else {
+		result = morse.ToMorse(input)
+	}
+
+	return result, nil
+}
